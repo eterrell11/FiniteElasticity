0764ea318da1840d797518465abccea90241e07f
Edward Terrell <terrell1@ad.unc.edu>
2022-01-31 12:00:15 -0500

    Added PRM input
    The three-field code now accepts a .prm file that assigns values for E, nu, timestep and end time.

---
+++ three_field_PRMinput/CMakeFiles/three_field_PrmInput.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/three_field_PrmInput.dir/three_field_PrmInput.cc.o"
+  "three_field_PrmInput"
+  "three_field_PrmInput.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/three_field_PrmInput.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
