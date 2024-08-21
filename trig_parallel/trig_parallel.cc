15a0c5e1e130e684174a761ac85bdd70afad809e
Edward Terrell <77814935+eterrell11@users.noreply.github.com>
2024-08-21 11:23:05 -0400

    fix

--- trig_parallel/trig_parallel.cc
+++ trig_parallel/trig_parallel.cc
@@ -1168,7 +1168,7 @@ namespace NonlinearElasticity
 		double mu;
 
 		unsigned int height;
-		unsigned int n_ref;
+		unsigned int n_ref
 
 		double tau;
 		int max_it;
@@ -1243,6 +1243,7 @@ namespace NonlinearElasticity
 		linfty_p_eps_vec.reserve(max_it);
 		height = 6;
 		for ( int ref_step = 0; ref_step < max_it; ++ref_step) {
+			n_ref = parameters.n_ref;
 			for (int i = 0; i < ref_step; ++i) {
 				dt *= 0.5;
 				n_ref += 1;
