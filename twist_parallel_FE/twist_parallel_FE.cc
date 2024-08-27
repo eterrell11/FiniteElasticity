13cfae60175aa8c4d2e59d2d17d0e1e26f6add49
Edward Terrell <77814935+eterrell11@users.noreply.github.com>
2024-08-27 00:48:47 -0400

    back to BlockJacobi

--- twist_parallel_FE/twist_parallel_FE.cc
+++ twist_parallel_FE/twist_parallel_FE.cc
@@ -2148,7 +2148,7 @@ template <int dim>
 		LA::MPI::PreconditionAMG preconditioner_S_comp;
 		preconditioner_S_comp.initialize(Pp, data);
 
-		PETScWrappers::PreconditionSOR preconditioner_S_in;
+		PETScWrappers::PreconditionBlockJacobi preconditioner_S_in;
 		preconditioner_S_in.initialize(Pp);
 
 		const InverseMatrix<LA::MPI::SparseMatrix, PETScWrappers::PreconditionBlockJacobi>
