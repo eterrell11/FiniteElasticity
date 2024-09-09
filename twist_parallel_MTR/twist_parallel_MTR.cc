66802a00425e3761e9f8448a8888e3ab324c915f
Edward Terrell <77814935+eterrell11@users.noreply.github.com>
2024-09-09 14:45:48 -0400

    fix typo

--- twist_parallel_MTR/twist_parallel_MTR.cc
+++ twist_parallel_MTR/twist_parallel_MTR.cc
@@ -1928,7 +1928,7 @@ template <int dim>
 						pk1_dev = get_pk1_dev(FF, mu, Jf, HH);
 						pk1_dev_tilde = pk1_dev;
 						HH_tilde = HH;
-						cout << pk1_dev_tilde << std::endl;
+						std::cout << pk1_dev_tilde << std::endl;
 
 					}
 					else 
@@ -1945,7 +1945,7 @@ template <int dim>
 
 						HH_tilde = 0.5 * (HH + old_HH);
 						pk1_dev_tilde = 0.5 * (pk1_dev + old_pk1_dev);
-						cout << pk1_dev_tilde << std::endl;
+						std::cout << pk1_dev_tilde << std::endl;
 					}
 
 
