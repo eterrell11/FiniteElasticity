883f61e9451f01475502c437dc3a9288f2d3b608
Edward Terrell <77814935+eterrell11@users.noreply.github.com>
2024-09-09 23:45:19 -0400

    split pressure term

--- twist_parallel_MTR/twist_parallel_MTR.cc
+++ twist_parallel_MTR/twist_parallel_MTR.cc
@@ -1858,7 +1858,7 @@ template <int dim>
 		Tensor<2, dim> pk1_dev_tilde;
 
 	
-		//double temp_pressure;
+		double temp_pressure;
 		Tensor<1,dim> un;
 		Tensor<1,dim> old_un;
 
@@ -1915,6 +1915,7 @@ template <int dim>
 				
 					if (MTR_counter==0)
 					{
+						temp_pressure = 0;
 						FF = get_real_FF(displacement_grads[q]);
 						Jf = get_Jf(FF);
 						HH = get_HH(FF, Jf);
@@ -1925,6 +1926,7 @@ template <int dim>
 					}
 					else 
 					{
+						temp_pressure = sol_vec_pressure[q];
 						FF = get_real_FF(tmp_displacement_grads[q]);
 						Jf = get_Jf(FF);
 						old_HH = get_HH(FF, Jf);
@@ -1950,12 +1952,12 @@ template <int dim>
 						for (const unsigned int j : fe_values.dof_indices())
 						{
 							cell_mass_matrix(i, j) += (scalar_product(Grad_u_i, (HH_tilde)*fe_values[Pressure].value(j, q)) - //Kup
-								dt * N_p_i * scalar_product(HH, fe_values[Velocity].gradient(j, q))) * fe_values.JxW(q);
+								0.5 * dt * N_p_i * scalar_product(HH, fe_values[Velocity].gradient(j, q))) * fe_values.JxW(q);
 							cell_preconditioner_matrix(i,j) += (1./kappa * N_p_i * fe_values[Pressure].value(j,q) +
 								(HH)*Grad_p_i * (HH * fe_values[Pressure].gradient(j,q) )) * fe_values.JxW(q);
 
 						}
-						cell_rhs(i) += (-scalar_product(Grad_u_i, pk1_dev_tilde) +
+						cell_rhs(i) += (-scalar_product(Grad_u_i, pk1_dev_tilde + 0.5 * temp_pressure * HH_tilde) +
 							rho_0 * N_u_i * rhs_values[q] +
 							N_p_i * (Jf - 1.0)) * fe_values.JxW(q);
 					}
@@ -2169,7 +2171,6 @@ template <int dim>
 
 
 		energy_RHS = 0;
-
 
 		FEValues<dim> fe_values(*mapping_ptr,
 			*fe_ptr,
