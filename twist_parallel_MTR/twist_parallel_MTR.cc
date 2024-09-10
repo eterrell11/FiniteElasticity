205fc8f3d0b01a6d3382ae9a84c8502211765592
Edward Terrell <77814935+eterrell11@users.noreply.github.com>
2024-09-10 00:36:38 -0400

    fix

--- twist_parallel_MTR/twist_parallel_MTR.cc
+++ twist_parallel_MTR/twist_parallel_MTR.cc
@@ -1951,13 +1951,13 @@ template <int dim>
 						auto Grad_p_i = fe_values[Pressure].gradient(i, q);
 						for (const unsigned int j : fe_values.dof_indices())
 						{
-							cell_mass_matrix(i, j) += (1.-0.5 * MTR_counter) * scalar_product(Grad_u_i, (HH_tilde)*fe_values[Pressure].value(j, q)) - //Kup
+							cell_mass_matrix(i, j) += ((1.-0.5 * MTR_counter) * scalar_product(Grad_u_i, (HH_tilde)*fe_values[Pressure].value(j, q)) - //Kup
 								dt * N_p_i * scalar_product(HH, fe_values[Velocity].gradient(j, q))) * fe_values.JxW(q);
 							cell_preconditioner_matrix(i,j) += (1./kappa * N_p_i * fe_values[Pressure].value(j,q) +
 								(HH)*Grad_p_i * (HH * fe_values[Pressure].gradient(j,q) )) * fe_values.JxW(q);
 
 						}
-						cell_rhs(i) += (-scalar_product(Grad_u_i, pk1_dev_tilde + 0.5 *MTR_counter temp_pressure * HH_tilde) +
+						cell_rhs(i) += (-scalar_product(Grad_u_i, pk1_dev_tilde + (0.5 *MTR_counter * temp_pressure * HH_tilde)) +
 							rho_0 * N_u_i * rhs_values[q] +
 							N_p_i * (Jf - 1.0)) * fe_values.JxW(q);
 					}
