b318c0da011d4f49d997ddbe40126f922126220f
Edward Terrell <77814935+eterrell11@users.noreply.github.com>
2024-10-01 23:59:32 -0400

    change IV for box

--- box_parallel_MTR/box_parallel_MTR.cc
+++ box_parallel_MTR/box_parallel_MTR.cc
@@ -740,8 +740,8 @@ template <class PreconditionerType>
 			Vector<double>& values) const
 	{
 		Assert(values.size() == (dim + 1), ExcDimensionMismatch(values.size(), dim + 1));
-		values[0] = -velocity * std::sin(M_PI * p[dim - 1] / 12.) * p[1];
-		values[1] = velocity * std::sin(M_PI * p[dim - 1] / 12.) * p[0];
+		//values[0] = -velocity * std::sin(M_PI * p[dim - 1] / 12.) * p[1];
+		values[1] = -velocity * std::sin(M_PI * p[dim - 1] / 2.) * p[0];
 		if (dim == 3) {
 			values[2] = 0;
 		}
