0117a491057eb1a1093a066f3a9b965b7617cdd4
Edward Terrell <77814935+eterrell11@users.noreply.github.com>
2024-10-02 01:15:40 -0400

    revert

--- box_parallel_MTR/box_parallel_MTR.cc
+++ box_parallel_MTR/box_parallel_MTR.cc
@@ -2105,7 +2105,7 @@ template <int dim>
 
 				fe_values[Velocity].get_function_gradients(relevant_solution, displacement_grads);
 				fe_values[Velocity].get_function_values(relevant_solution_dot, sol_vec_velocity);
-				fe_values[Pressure].get_function_values(extra_solution, sol_vec_pressure);
+				fe_values[Pressure].get_function_values(relevant_solution, sol_vec_pressure);
 
 
 				//present_time -= 0.5 * dt;
