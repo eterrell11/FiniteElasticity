1368ce4bb3fd2a71ccde9e32d706bc644e7e103d
Edward Terrell <77814935+eterrell11@users.noreply.github.com>
2024-10-06 15:47:19 -0400

    shift FExt

--- box_parallel_MTR/box_parallel_MTR.cc
+++ box_parallel_MTR/box_parallel_MTR.cc
@@ -1938,9 +1938,9 @@ template <int dim>
 				}
 				else
 				{
-					present_time -= dt;
+					present_time -= 0.5 * dt;
 					right_hand_side.rhs_vector_value_list(fe_values.get_quadrature_points(), rhs_values, parameters.BodyForce, present_time, mu, kappa);
-					present_time += dt;
+					present_time += 0.5 * dt;
 				}
 
 				for (const unsigned int q : fe_values.quadrature_point_indices())
