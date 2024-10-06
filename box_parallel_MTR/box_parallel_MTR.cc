6f27928bd22f49a35c44040f5379ae38a64752a7
Edward Terrell <77814935+eterrell11@users.noreply.github.com>
2024-10-06 14:25:56 -0400

    adjust time of FExt evaluation

--- box_parallel_MTR/box_parallel_MTR.cc
+++ box_parallel_MTR/box_parallel_MTR.cc
@@ -1930,10 +1930,16 @@ template <int dim>
 				fe_values[Velocity].get_function_values(relevant_old_solution, old_sol_vec_displacement);
 				fe_values[Velocity].get_function_gradients(relevant_solution_extrap, tmp_displacement_grads);
 
-
+				if (MTR_counter==1)
+				{
 				present_time -= 0.5 * dt;
 				right_hand_side.rhs_vector_value_list(fe_values.get_quadrature_points(), rhs_values, parameters.BodyForce, present_time, mu, kappa);
 				present_time += 0.5 * dt;
+				}
+				else
+				{
+					right_hand_side.rhs_vector_value_list(fe_values.get_quadrature_points(), rhs_values, parameters.BodyForce, present_time, mu, kappa);
+				}
 
 				for (const unsigned int q : fe_values.quadrature_point_indices())
 				{
