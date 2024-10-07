1ada9a46ef299860341eb36ed63215e1cde103f2
Edward Terrell <77814935+eterrell11@users.noreply.github.com>
2024-10-07 11:58:44 -0400

    back to midpoint for pressure

--- box_parallel_MTR/box_parallel_MTR.cc
+++ box_parallel_MTR/box_parallel_MTR.cc
@@ -1954,7 +1954,7 @@ template <int dim>
 				
 					if (MTR_counter==0)
 					{
-						temp_pressure = 0;
+						//temp_pressure = 0;
 						// FF = get_real_FF(tmp_displacement_grads[q]);
 						// Jf = get_Jf(FF);
 						// HH = get_HH(FF, Jf);
@@ -1971,7 +1971,7 @@ template <int dim>
 					}
 					else 
 					{
-						temp_pressure = sol_vec_pressure[q];
+						//temp_pressure = sol_vec_pressure[q];
 						FF = get_real_FF(tmp_displacement_grads[q]);
 						Jf = get_Jf(FF);
 						old_HH = get_HH(FF, Jf);
@@ -1982,7 +1982,7 @@ template <int dim>
 						HH = get_HH(FF, Jf);
 						pk1_dev = get_pk1_dev(FF, mu, Jf, HH);
 
-						HH_tilde = old_HH; //
+						HH_tilde = 0.5 * (old_HH + HH); //
 						pk1_dev_tilde = 0.5 * (pk1_dev + old_pk1_dev); //old_pk1_dev ; //
 					}
 
@@ -1996,13 +1996,13 @@ template <int dim>
 						auto Grad_p_i = fe_values[Pressure].gradient(i, q);
 						for (const unsigned int j : fe_values.dof_indices())
 						{
-							cell_mass_matrix(i, j) += ((1. - MTR_counter * 0.5)*scalar_product(Grad_u_i, (HH_tilde)*fe_values[Pressure].value(j, q)) - //Kup
-								/*(1-0.5*double(MTR_counter)) */dt * N_p_i * scalar_product(HH, fe_values[Velocity].gradient(j, q))) * fe_values.JxW(q);
+							cell_mass_matrix(i, j) += ((0.5)*scalar_product(Grad_u_i, (HH_tilde)*fe_values[Pressure].value(j, q)) - //Kup
+								(1-0.5*double(MTR_counter)) * dt * N_p_i * scalar_product(HH, fe_values[Velocity].gradient(j, q))) * fe_values.JxW(q);
 							cell_preconditioner_matrix(i,j) += (1./kappa * N_p_i * fe_values[Pressure].value(j,q) +
 								(HH)*Grad_p_i * (HH * fe_values[Pressure].gradient(j,q) )) * fe_values.JxW(q);
 
 						}
-						cell_rhs(i) += (-scalar_product(Grad_u_i, pk1_dev_tilde + 0.5 * temp_pressure * HH) +
+						cell_rhs(i) += (-scalar_product(Grad_u_i, pk1_dev_tilde) +
 							 rho_0 * N_u_i * rhs_values[q] +
 							N_p_i * (Jf - 1.0)) * fe_values.JxW(q);
 					}
