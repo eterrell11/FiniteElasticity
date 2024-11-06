1fc540ef1bb9da4766daae2ec5c009b900fb29cb
Edward Terrell <77814935+eterrell11@users.noreply.github.com>
2024-11-06 12:19:07 -0500

    put P_comp back in for SBDF2

--- twist_parallel_MTR/twist_parallel_MTR.cc
+++ twist_parallel_MTR/twist_parallel_MTR.cc
@@ -2520,7 +2520,7 @@ template <int dim>
 		}
 		else
 		{
-			solver_S.solve(schur_complement, p, R.block(1), preconditioner_aS);
+			solver_S.solve(schur_complement, p, R.block(1), preconditioner_S_comp);
 		}
 		constraints.distribute(solution);
 
