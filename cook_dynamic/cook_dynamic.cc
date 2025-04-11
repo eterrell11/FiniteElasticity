334d31f62a46733a5813ce8591e6485f62fe395f
Edward Terrell <77814935+eterrell11@users.noreply.github.com>
2025-04-11 14:41:38 -0400

    lol

--- cook_dynamic/cook_dynamic.cc
+++ cook_dynamic/cook_dynamic.cc
@@ -883,7 +883,7 @@ namespace NonlinearElasticity
 			void
 			evaluate_vector_field
 			(const DataPostprocessorInputs::Vector<dim>& input_data,
-				scalar& computed_quantities) const override
+				double& computed_quantities) const override
 		{
 			AssertDimension(input_data.solution_gradients.size(),
 				computed_quantities.size());
